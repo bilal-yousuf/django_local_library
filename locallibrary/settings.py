436e6f8d67b9d2b58b9b15ab2dd195c0b9ff564c
Bilal Yousuf <bilal.yousuf610@gmail.com>
2020-02-20 14:38:52 -0500

    Updated settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'intense-tor-71350.herokuapp.com']
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'intense-tor-71350.herokuapp.com']
+
 
 
 # Application definition
