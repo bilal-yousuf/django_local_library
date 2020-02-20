19519ddb6f131ea900127c7c2d7440d78e8af611
Bilal Yousuf <bilal.yousuf610@gmail.com>
2020-02-20 03:13:22 -0500

    Added more Heroku deployment files

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-export DJANGO_DEBUG=False
+
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
