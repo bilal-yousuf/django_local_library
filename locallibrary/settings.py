4dac8675b2a3dc9e17f8fae5a3886f02be71a0ce
Bilal Yousuf <bilal.yousuf610@gmail.com>
2020-05-13 17:20:34 -0400

    Finally added an image! See index.html for reference and add to notes!

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -137,6 +137,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 ## STATIC FILES for site-wide static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
